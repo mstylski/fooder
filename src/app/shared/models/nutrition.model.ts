abf480b65e0d1e1d7d655eea32eda5ebc7f5fa69
Michał Stylski <tim@timrich.org>
2020-11-26 20:26:02 +0100

    calorie-calculator

---
+++ src/app/shared/models/nutrition.model.ts
@@ -0,0 +1,40 @@
+export interface NutritionModel {
+  name: string;
+  totalCalories: number;
+  totalKj: number;
+  quantity: number;
+  nutritionFacts: NutritionFacts;
+  timeToBurn: TimeToBurn;
+}
+
+export interface NutritionFacts {
+  totalFat: number;
+  totalFatPercentage: number;
+  saturatedFat: number;
+  polyunsaturatedFat: number;
+  monounsaturatedFat: number;
+  cholesterol: number;
+  totalCholesterolPercentage: number;
+  sodium: number;
+  totalSodiumPercentage: number;
+  totalCarbohydrate: number;
+  totalCarbohydratePercentage: number;
+  fiber: number;
+  totalFiberPercentage: number;
+  sugars: number;
+  protein: number;
+  calcium: number;
+  potassium: number;
+  alcohol?: number;
+  iron: number;
+  vitA: number;
+  vitC: number;
+  caffeine?: number;
+}
+
+export interface TimeToBurn {
+  swimming: number;
+  running: number;
+  cycling: number;
+  walking: number;
+}
